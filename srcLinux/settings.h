8383a921dbf72794ab2e7ec36b95b5ce7a256ca5
amchess <amchess@libero.it>
2017-05-20 16:00:16 +0200

    From Merge pull request #388 from snicolet/ring3' May 17, 2017

---
+++ srcLinux/settings.h
@@ -0,0 +1,20 @@
+#include "stddef.h"
+
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+
+struct settings {
+  int mask;
+  int numa_enabled;
+  size_t tt_size;
+  size_t num_threads;
+  int large_pages;
+};
+
+extern struct settings settings, delayed_settings;
+
+void process_delayed_settings(void);
+
+#endif
+
